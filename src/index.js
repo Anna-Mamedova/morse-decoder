3b9ed18269d9f5a3c635b3f5e45b800609681488
Anna-Mamedova <mamedi289@gmail.com>
2023-01-19 19:38:26 +0200

    feat: add morse decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,58 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = []
+    let newLetters = []
+    let morse = []
+    for(let i = 0; i < expr.length; i += 10){
+        letters.push(expr.slice(i, i + 10))
+    }
+    for(let i = 0; i < letters.length; i++){
+        let letter = letters[i]
+        for(let j = 0; j < 10; j++){
+            if(letter[j] === '*'){
+                newLetters.push('**********')
+                break
+            }
+            if(letter[j] === '1'){
+                newLetters.push(letter.slice(j))
+                break
+            }
+        }
+    }
+    for(let i = 0; i < newLetters.length; i++){
+        for(let j = 0; j <= newLetters[i].length; j += 2){
+            if(newLetters[i][j] === '*'){
+                morse.push('**********')
+                j += 8
+            }
+            if(j === newLetters[i].length && i < newLetters.length - 1){
+                morse.push(' ')
+            }
+            if(newLetters[i][j] + newLetters[i][j + 1] === '10'){
+                morse.push('.')
+            }
+            if(newLetters[i][j] + newLetters[i][j + 1] === '11'){
+                morse.push('-')
+            }
+        
+        }
+        
+    }
+    morse = morse.join('').split(' ')
+    let result = ''
+    for(let i = 0; i < morse.length; i++){
+        if(morse[i].includes('*')){
+            result += ' '
+        }
+        for(let j = 0; j < 36; j++){
+            if(Object.keys(MORSE_TABLE)[j] === morse[i]){
+                result += Object.values(MORSE_TABLE)[j]
+            }
+        }
+    }
+
+    return result
 }
 
 module.exports = {
